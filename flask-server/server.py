dceef00eff7a3bd126bd5d17450841568ed26f4f
Imran Rehman <imran.rehman@mail.utoronto.ca>
2022-10-22 17:58:28 -0400

    added modularity to routes

--- flask-server/server.py
+++ flask-server/server.py
@@ -9,26 +9,27 @@ app = Flask(__name__)
 CORS(app)
 
 
+def extract_article(url):
+    subResult = requests.get(url)
+    subDoc = BeautifulSoup(subResult.text, "html.parser")
+    article = ""
+
+    for a in subDoc.find_all('p', {"class": None}):
+        # gets all <p> up until end of article denoted by Audience Relations
+        if (a.text.find("Audience Relations, CBC") != -1):
+            break
+        else:
+            article = article + a.text
+
+    return article + "link_to_article:" + url
+
+
 @app.route('/worldnews')
 def worldnews():
     url = "https://www.cbc.ca/news/world"
 
     result = requests.get(url)
     doc = BeautifulSoup(result.text, "html.parser")
-
-    def extract_article(url):
-        subResult = requests.get(url)
-        subDoc = BeautifulSoup(subResult.text, "html.parser")
-        article = ""
-
-        for a in subDoc.find_all('p', {"class": None}):
-            # gets all <p> up until end of article denoted by Audience Relations
-            if (a.text.find("Audience Relations, CBC") != -1):
-                break
-            else:
-                article = article + a.text
-
-        return article + "link_to_article:" + url
 
     # extracts all links to news article from home page to be later accessed for article extraction
     urlList = []
@@ -51,20 +52,6 @@ def canadanews():
     result = requests.get(url)
     doc = BeautifulSoup(result.text, "html.parser")
 
-    def extract_article(url):
-        subResult = requests.get(url)
-        subDoc = BeautifulSoup(subResult.text, "html.parser")
-        article = ""
-
-        for a in subDoc.find_all('p', {"class": None}):
-            # gets all <p> up until end of article denoted by Audience Relations
-            if (a.text.find("Audience Relations, CBC") != -1):
-                break
-            else:
-                article = article + a.text
-
-        return article + "link_to_article:" + url
-
     # extracts all links to news article from home page to be later accessed for article extraction
     urlList = []
     allArticles = []
@@ -86,25 +73,30 @@ def politicsnews():
     result = requests.get(url)
     doc = BeautifulSoup(result.text, "html.parser")
 
-    def extract_article(url):
-        subResult = requests.get(url)
-        subDoc = BeautifulSoup(subResult.text, "html.parser")
-        article = ""
-
-        for a in subDoc.find_all('p', {"class": None}):
-            # gets all <p> up until end of article denoted by Audience Relations
-            if (a.text.find("Audience Relations, CBC") != -1):
-                break
-            else:
-                article = article + a.text
-
-        return article + "link_to_article:" + url
-
     # extracts all links to news article from home page to be later accessed for article extraction
     urlList = []
     allArticles = []
     for a in doc.find_all('a', href=True, ):
         if (a['href'].find('/news/politics') != -1):
+            print("Found the URL:", a['href'])
+            urlList.append(a['href'])
+
+            allArticles.append(extract_article(
+                "https://www.cbc.ca" + a['href']))
+
+    return json.dumps(allArticles)
+
+
+@app.route('/climatenews')
+def politicsnews():
+    url = "https://www.cbc.ca/news/climate"
+    result = requests.get(url)
+    doc = BeautifulSoup(result.text, "html.parser")
+    # extracts all links to news article from home page to be later accessed for article extraction
+    urlList = []
+    allArticles = []
+    for a in doc.find_all('a', href=True, ):
+        if (a['href'].find('/news/climate') != -1):
             print("Found the URL:", a['href'])
             urlList.append(a['href'])
 
